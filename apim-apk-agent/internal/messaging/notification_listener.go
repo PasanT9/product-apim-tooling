2d825341cf7fd69812abba7d5ec23ce4ce92929e
sgayangi <gayangi.18@cse.mrt.ac.lk>
2026-02-10 09:31:44 +0530

    Fix variable and method naming

--- apim-apk-agent/internal/messaging/notification_listener.go
+++ apim-apk-agent/internal/messaging/notification_listener.go
@@ -552,7 +552,7 @@ func handlePolicyEvents(data []byte, eventType string, c client.Client) {
 		} else if strings.EqualFold(policyEvent.PolicyType, "SUBSCRIPTION") {
 			logger.LoggerMessaging.Infof("Policy: %s for policy type: %s", policyEvent.PolicyName, policyEvent.PolicyType)
 			managementserver.DeleteSubscriptionPolicy(policyEvent.PolicyName, policyEvent.TenantDomain)
-			crName := k8sclient.PrepareSubscriptionPolicyCRName(policyEvent.PolicyName, policyEvent.TenantDomain)
+			crName := k8sclient.PrepareRateLimitPolicyCRName(policyEvent.PolicyName, policyEvent.TenantDomain)
 			k8sclient.UnDeploySubscriptionRateLimitPolicyCR(crName, c)
 			k8sclient.UndeploySubscriptionAIRateLimitPolicyCR(crName, c)
 			ratelimitPolicies := managementserver.GetAllRateLimitPolicies()
